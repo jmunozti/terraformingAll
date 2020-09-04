859b10c05c1c5c639ab2dd30d1cc8278d9a342d5
Jorge Muñoz <jmunozti@gmail.com>
2020-09-04 15:20:54 -0500

    Cleaning up

--- tf/rta.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_route_table_association" "rtb-0afa29b53d4306560-rtbassoc-00d6c4f9c4ef4de71" {
-    route_table_id = "rtb-0afa29b53d4306560"
-    subnet_id = "subnet-06d1f0485318c7520"
-}
-
